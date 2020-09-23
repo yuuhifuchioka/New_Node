db6dbe3a0f1025436f3fd8a4600de1bd21dea084
yuuhifuchioka <69574777+yuuhifuchioka@users.noreply.github.com>
2020-09-23 17:54:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/p/', (req, res) => {
           
           //Sharpによる画像変換
           sharp(decode)
-                    .png({compressionLevel: 0})
+                    .png()
                     .toFile('/tmp/tmp.png');
           
           //TIFF形式に変換したファイルをエンコード
