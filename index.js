8b463f5e5dfa71beadd85db58e5b5294f9a5e950
yuuhifuchioka <69574777+yuuhifuchioka@users.noreply.github.com>
2020-09-23 15:01:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ app.post('/p/', (req, res) => {
                     
                     //res.json({method: data});
           });
-          encode = fs.existsSync('/tmp/tmp.tiff');
-          encode = sharp.versions;
+          //encode = fs.existsSync('/tmp/tmp.tiff');
+          encode = sharp('/tmp/tmp.png').toBuffer();
           //エンコード文字列をJSON形式で返す
           res.json({method: encode});
           }
